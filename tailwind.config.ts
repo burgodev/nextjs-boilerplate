70cdeb53dad7a2ec7dd7ce6c03a864646c7d26f1
filipe.burgonovo <burgodev@gmail.com>
2024-08-22 13:05:04 -0300

    chore: configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,10 +9,60 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        sans: ["Neue Montreal", "sans-serif"],
+      },
+      fontSize: {
+        sm: "0.8rem",
+        base: "1rem",
+        md: "1.5rem",
+        lg: "2rem",
+        xl: "2.5rem",
+        "2xl": "3rem",
+        "3xl": "4rem",
+        "4xl": "5rem",
+        "5xl": "6.25rem",
+      },
+      screens: {
+        xs: "375px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        scaleUp: {
+          "0%": { transform: "scale(0.85)", opacity: "0" },
+          "100%": { transform: "scale(1)", opacity: "1" },
+        },
+        scaleDown: {
+          "0%": { transform: "scale(1.15)", opacity: "0" },
+          "100%": { transform: "scale(1)", opacity: "1" },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 1s ease-in-out forwards",
+        scaleUp: "scaleUp 1s ease-in-out forwards",
+        scaleDown: "scaleDown 1s ease-in-out forwards",
+      },
+      width: {
+        "svg-xs": "80px",
+        "svg-sm": "120px",
+        "svg-md": "180px",
+        "svg-lg": "240px",
+        "svg-xl": "300px",
+      },
+      height: {
+        "svg-xs": "40px",
+        "svg-sm": "60px",
+        "svg-md": "60px",
+        "svg-lg": "60px",
+        "svg-xl": "60px",
       },
     },
   },
